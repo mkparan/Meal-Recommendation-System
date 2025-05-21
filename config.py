679d031500f56828ac6709d1d14d02716b8cc487
mkparan <mdspot13@gmail.com>
2025-05-21 17:01:43 +0800

    Updated some dependencies

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://meal_user:meal_pass@localhost/meal_recommender'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://localhost/meal_recommender")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
