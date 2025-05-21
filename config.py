38f7b7298c9291f3f56becb475b13073e2e2c212
mkparan <mdspot13@gmail.com>
2025-05-21 19:09:44 +0800

    added some more recipes

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://localhost/meal_recommender")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# postgresql://meal_user:meal_pass@localhost/meal_recommender
+# postgresql://localhost/meal_recommender
